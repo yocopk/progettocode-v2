a7e0417389332443a5c573589013a4cc12d0372c
ValerioErnestoColibro <valerio.colibro@gmail.com>
2024-04-23 10:01:44 +0200

    feat(valeriocolibro): added new html and md files

---
+++ valerio_colibro/sheets/CSS.md
@@ -0,0 +1,5 @@
+# CSS
+
+## Definizione
+
+CSS, acronimo di "Cascading Style Sheets" (Fogli di Stile a Cascata), è un linguaggio utilizzato per definire l'aspetto e la formattazione di un documento HTML. Con CSS, è possibile controllare la presentazione visuale di elementi HTML, come il colore, la dimensione, il tipo di carattere, il layout e altri aspetti del design. CSS separa la struttura del contenuto (definita in HTML) dalla sua presentazione, consentendo agli sviluppatori di creare siti web più flessibili, coerenti e accessibili.
---
+++ valerio_colibro/sheets/DOM.gd
@@ -0,0 +1,7 @@
+# DOM
+
+## Definizione
+
+In informatica, "DOM" è un acronimo che sta per "Document Object Model", che in italiano si traduce come "Modello a Oggetti del Documento". 
+Il DOM è un'interfaccia di programmazione delle applicazioni (API) per documenti HTML, XML e SVG. Il DOM rappresenta la struttura di un documento come un albero dove ogni nodo rappresenta una parte del documento.
+In altre parole, il Document Object Model (DOM) è una rappresentazione gerarchica e strutturata di un documento web, che consente di accedere e manipolare il contenuto, la struttura e lo stile di un documento attraverso il codice JavaScript. Il DOM consente ai programmatori di scrivere script che interagiscono con gli elementi di una pagina web, aggiungendo, modificando o eliminando dinamicamente gli elementi e il loro contenuto.
