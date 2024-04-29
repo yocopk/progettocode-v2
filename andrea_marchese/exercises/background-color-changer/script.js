aca67b77596574ba3d659f7d0347d5fa0a573323
yocopk <yocopk@gmail.com>
2024-04-29 17:46:39 +0200

    feat(andrea_marchese): js exercises fullfilled

---
+++ andrea_marchese/exercises/background-color-changer/script.js
@@ -0,0 +1,5 @@
+const colorDropdown = document.getElementById("color-dropdown");
+
+colorDropdown.addEventListener("change", function () {
+  document.body.style.backgroundColor = colorDropdown.value;
+});
