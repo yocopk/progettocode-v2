8c4738acf01a24f681df1689ca1ce79a8b84039b
GiovanniLR93 <giovanni_la_rosa@hotmail.it>
2024-05-02 01:21:03 +0200

    feat(giovanni_larosa)added exercises and javascript's lessons

---
+++ giovanni_larosa/website/esercizi/js/esercizio3.js
@@ -0,0 +1,12 @@
+var apriti = document.getElementById("paragrafodue");
+const passaqui = document.getElementById("paragrafouno");
+function mostra() {
+  paragrafodue.style.display = "block";
+}
+function cambia() {
+  apriti.textContent = "sono cambiato";
+  console.log;
+}
+passaqui.addEventListener("click", mostra);
+
+apriti.addEventListener("click", cambia);
---
+++ giovanni_larosa/website/esercizi/js/esercizio9.js
@@ -0,0 +1,6 @@
+var titolo = document.getElementById("titolo");
+var body = document.body;
+function cambiacolore() {
+  titolo.style.color = "red";
+}
+titolo.addEventListener("mouseover", cambiacolore);
