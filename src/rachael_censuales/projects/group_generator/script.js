77cb5d5c3ebd291cad8baaa9985218f139d38202
Cenael <recyce@hotmail.it>
2024-05-10 17:40:43 +0200

    feat(config): modifie files

--- src/rachael_censuales/projects/group_generator/script.js
+++ src/rachael_censuales/projects/group_generator/script.js
@@ -1,34 +1,32 @@
+/** @format */
+
 const studentList = [];
 
 let groupsList = [];
 
-const nodeAddButton = document.getElementById("addButton");
-const nodeInput = document.getElementById("input");
-const nodeList = document.getElementById("list");
-const nodeRandomButton = document.getElementById("nodeRandomButton");
-let menu = document.getElementById("menu");
+const nodeAddButton = document.getElementById('addButton');
+const nodeInput = document.getElementById('input');
+const nodeList = document.getElementById('list');
+const nodeRandomButton = document.getElementById('nodeRandomButton');
+let menu = document.getElementById('menu');
 
 let groupsList = [];
 
-
-const group = document.getElementById("groups");
+const group = document.getElementById('groups');
 
 function onClickAddButton() {
-  const newNode = document.createElement("li");
+  const newNode = document.createElement('li');
   newNode.id = nodeInput.value;
   newNode.textContent = nodeInput.value;
   nodeList.appendChild(newNode);
   studentList.push(nodeInput.value.toLowerCase());
   groupsList.push(nodeInput.value);
-  nodeInput.value = "";
+  nodeInput.value = '';
   nodeAddButton.disabled = true;
 }
 
 function onChangeInput() {
-  if (
-    nodeInput.value !== "" &&
-    !studentList.includes(nodeInput.value.toLowerCase())
-  ) {
+  if (nodeInput.value !== '' && !studentList.includes(nodeInput.value.toLowerCase())) {
     nodeAddButton.disabled = false;
   } else {
     nodeAddButton.disabled = true;
@@ -42,7 +40,7 @@ function groupCalculator() {
 }
 
 function onClickRandomButton() {
-  group.innerHTML = "";
+  group.innerHTML = '';
   let groupSize = menu.value;
   let groupsNumber = groupCalculator();
   let studentsCopy = [...groupsList];
@@ -56,14 +54,15 @@ function onClickRandomButton() {
 
   let i = 0;
   while (i < groupsNumber) {
-    const newNodeUl = document.createElement("ul");
-    newNodeUl.textContent = "Group " + (i + 1);
+    const newNodeUl = document.createElement('ul');
+    newNodeUl.textContent = 'Group ' + (i + 1);
     group.appendChild(newNodeUl);
     let j = 0;
+  }
 
-    while (j < groupSize && studentsCopy.length > 0) {
-      let randomNumber = Math.floor(Math.random() * studentsCopy.length);
-      let randomElement = studentsCopy[randomNumber];
+  while (j < groupSize && studentsCopy.length > 0) {
+    let randomNumber = Math.floor(Math.random() * studentsCopy.length);
+    let randomElement = studentsCopy[randomNumber];
 
     while (j < groupSize) {
       let randomNumber = Math.floor(Math.random() * groupsList.length);
@@ -84,21 +83,16 @@ function onClickRandomButton() {
   }
 }
 
-
 function onEnterPress(event) {
-  if (
-    event.key === "Enter" &&
-    nodeInput.value !== "" &&
-    !studentList.includes(nodeInput.value.toLowerCase())
-  ) {
+  if (event.key === 'Enter' && nodeInput.value !== '' && !studentList.includes(nodeInput.value.toLowerCase())) {
     onClickAddButton();
   }
 }
 
-nodeInput.addEventListener("keypress", onEnterPress);
+nodeInput.addEventListener('keypress', onEnterPress);
 
-nodeAddButton.addEventListener("click", onClickAddButton);
+nodeAddButton.addEventListener('click', onClickAddButton);
 
-nodeInput.addEventListener("input", onChangeInput);
+nodeInput.addEventListener('input', onChangeInput);
 
-nodeRandomButton.addEventListener("click", onClickRandomButton);
+nodeRandomButton.addEventListener('click', onClickRandomButton);
