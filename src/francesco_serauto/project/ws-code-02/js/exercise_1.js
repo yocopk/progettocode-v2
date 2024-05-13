18bc90195cdfce340d8ff7a3b4f69b97791b84cd
Mx444 <mybbtester@gmail.com>
2024-05-13 20:41:00 +0200

    fix(website): fix format , fix eslint

--- src/francesco_serauto/project/ws-code-02/js/exercise_1.js
+++ src/francesco_serauto/project/ws-code-02/js/exercise_1.js
@@ -1,19 +1,27 @@
 /** @format */
 
 'use strict';
+
+const btnIncrementa = document.getElementById('btnIncrementa');
+const btnReset = document.getElementById('btnReset');
+const btnElimina = document.getElementById('btnElimina');
+
 //AutoElimina Bottone
 function rimuoviBottone() {
   const bottoneDaEliminare = document.getElementById('autoElimina');
   bottoneDaEliminare.remove(bottoneDaEliminare);
 }
+btnElimina.addEventListener('click', rimuoviBottone);
 //Incrementa Paragrafo
 let valore = 0;
 function incrementaCounter() {
   valore++;
   document.getElementById('counter').textContent = valore;
 }
+btnIncrementa.addEventListener('click', incrementaCounter);
 //Reset Paragrafo
 function resetCounter() {
   valore = 0;
   document.getElementById('counter').textContent = valore;
 }
+btnReset.addEventListener('click', resetCounter);
