da9d3d04e490d905158fa43030d34ae5fa26bd97
Gabriele Lo Re <gabrolr70@gmail.com>
2024-05-14 11:35:01 +0200

    feat(gabriele_lore): Created task list script

---
+++ src/gabriele_lore/projects/task-list/script.js
@@ -0,0 +1,46 @@
+class Application {
+    #todos = []; // {descrizione: , done: true/false, id: }
+ 
+    addTask(descr)
+    {
+        this.#todos = [...this.#todos, {descrizione: descr, done: false, id: Math.random()}];
+    }
+    removeTask(idTask)
+    {
+        function onFilter(task)
+        {
+            return task.id !== idTask;
+        }
+        this.#todos = this.#todos.filter(onFilter);
+    }
+    updateTask(idTask, descrNuova)
+    {
+        function onMap(task)
+        {
+            if(task.id === idTask)
+            {
+                task.descrizione = descrNuova;
+            }
+            return task;
+        }
+        this.#todos = this.#todos.map(onMap);
+    }
+    toggle(idTask)
+    {       
+        function onMap(task)
+        {
+            if(task.id === idTask)
+            {
+                task.done = !task.done;
+            }
+            return task;
+        }
+        this.#todos = this.#todos.map(onMap);
+    }
+    getTodos()
+    {
+        return this.#todos;
+    }
+ 
+}
+const app = new Application();
