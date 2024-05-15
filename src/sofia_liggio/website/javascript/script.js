f94b0be664334c33423da334dc01ac891404f2a2
Riccardo Genova <r.genova@bitrocket.dev>
2024-05-15 18:21:37 +0200

    fix(config): fix js errors

--- src/sofia_liggio/website/javascript/script.js
+++ src/sofia_liggio/website/javascript/script.js
@@ -2,26 +2,26 @@
 
 //esercizio 1//
 
-function elimina() {
-  const DeletedButton = document.getElementById('delete');
+// function elimina() {
+//   const DeletedButton = document.getElementById('delete');
 
-  DeletedButton.remove(elimina);
-}
+//   DeletedButton.remove(elimina);
+// }
 
 let contatore = 0;
 
-function incrementa() {
-  contatore++;
+// function incrementa() {
+//   contatore++;
 
-  document.getElementById('amount').textContent = contatore;
-}
+//   document.getElementById('amount').textContent = contatore;
+// }
 // reset //
 
-function reset() {
-  contatore = 0;
+// function reset() {
+//   contatore = 0;
 
-  document.getElementById('amount').textContent = contatore;
-}
+//   document.getElementById('amount').textContent = contatore;
+// }
 
 //esercizio 2//
 const hideButton = document.getElementById('hide');
@@ -41,5 +41,5 @@ showButton.addEventListener('click', function contentHidden() {
 var changeColor = document.getElementById('content');
 
 changeColor.addEventListener('mouseover', function changeColor() {
-  content.style.color = red;
+  content.style.color = 'red';
 });
